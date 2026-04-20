e59380a85edc8b06e56258f20a8df09441f9d738
enricjaen06 <448417.clot@fje.edu>
2026-04-20 17:14:49 +0000

    fix pizza IVA 20 preu 10

--- pizza.php
+++ pizza.php
@@ -1,28 +1,30 @@
 <html>
-	<head>
-    	<meta content="text/html; charset=UTF-8" http-equiv="content-type">
-		<title>FES LA TEVA PIZZA - Prova competencial pj9f5a15</title>
-	</head>
-	<body>
-		<?php
-        	$preu = 10;
-          	$IVA=20;
-          	if (empty($_GET['ingr'])) {
-            	$pvp=((100+$IVA)/100)*$preu;
-	    		echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
-			}          
-          	else {
-            	$pvp=((100+$IVA)/100)*($preu+count($_GET['ingr']));
-	     		echo "Has demanat la pizza bàsica amb:<br> ";
-	     		foreach ($_GET['ingr'] as $ingredient) {
-	          		if ($ingredient) {
-		        		echo "* $ingredient<br>";
-		   			}
-	      		}
-	      		echo "El preu és $pvp € (IVA inclòs)<br>";
-			}
-			echo "<br>";
-     	?>
-     	<a href="index.html">Torna a index.html</a><br>
-	</body>
+        <head>
+        <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+                <title>FES LA TEVA PIZZA - Prova competencial pj9f5a15</title>
+        </head>
+        <body>
+                <?php
+                $preu = 10;
+                $IVA = 20;
+
+                if (empty($_GET['ingr'])) {
+                        $pvp = ((100 + $IVA) / 100) * $preu;
+                        echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
+                }
+                else {
+                        $pvp = ((100 + $IVA) / 100) * ($preu + count($_GET['ingr']));
+                        echo "Has demanat la pizza bàsica amb:<br> ";
+                        foreach ($_GET['ingr'] as $ingredient) {
+                                if ($ingredient) {
+                                        echo "* $ingredient<br>";
+                                }
+                        }
+                        echo "El preu és $pvp € (IVA inclòs)<br>";
+                }
+
+                echo "<br>";
+                ?>
+                <a href="index.html">Torna a index.html</a><br>
+        </body>
 </html>
