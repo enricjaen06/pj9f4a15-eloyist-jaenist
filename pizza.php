24d569f497bcd6abbaa2b22978938ce64afbae27
enricjaen06 <448417.clot@fje.edu>
2026-04-20 17:03:05 +0000

    prova examen IVA 20 preu 10

--- pizza.php
+++ pizza.php
@@ -5,8 +5,8 @@
 	</head>
 	<body>
 		<?php
-        	$preu = 12;
-          	$IVA=11;
+        	$preu = 20;
+          	$IVA=10;
           	if (empty($_GET['ingr'])) {
             	$pvp=((100+$IVA)/100)*$preu;
 	    		echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
